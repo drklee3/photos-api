6fb07bccb1307584a549bf7083ddc748009feacc
drklee3 <derrick@dlee.dev>
2022-01-01 12:02:58 -0800

    Add api token seed

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -1,52 +1,20 @@
 import { PrismaClient, Prisma } from '@prisma/client'
+import { generateApiToken } from '../src/utils/apiToken'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    username: 'Alice',
-    email: 'alice@prisma.io',
-    password: '$2a$10$TLtC603wy85MM./ot/pvEec0w2au6sjPaOmLpLQFbxPdpJH9fDwwS', // myPassword42
-    albums: {
-      create: [
-        {
-          title: 'Join the Prisma Slack',
-          photos: {
-            create: [
-              {
-                photo: {
-                  create: {
-                    title: 'a photo',
-                    description: 'hi',
-                  },
-                },
-              },
-            ],
-          },
-        },
-      ],
-    },
-  },
-  {
-    username: 'Nilu',
-    email: 'nilu@prisma.io',
-    password: '$2a$10$k2rXCFgdmO84Vhkyb6trJ.oH6MYLf141uTPf81w04BImKVqDbBivi', // random42
-  },
-  {
-    username: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    password: '$2a$10$lTlNdIBQvCho0BoQg21KWu/VVKwlYsGwAa5r7ctOV41EKXRQ31ING', // iLikeTurtles42
-  },
-]
-
 async function main() {
   console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
+
+  const token = generateApiToken()
+  const apiToken = await prisma.apiToken.create({
+    data: {
+      token,
+      allowedIPs: [],
+    },
+  })
+  console.log(`Created API token: ${apiToken.token}`)
+
   console.log(`Seeding finished.`)
 }
 
